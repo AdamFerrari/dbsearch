5fc03a6fb4f0fc1fe0f6aa01d2b512853e85c02d
AdamFerrari <aferrari@salsify.com>
2016-01-19 22:38:20 -0500

    Fast indexing, working.

--- dbidx/src/com/adam/Main.java
+++ dbidx/src/com/adam/Main.java
@@ -3,21 +3,31 @@ package com.adam;
 import java.util.HashMap;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.Arrays;
 
 public class Main {
-    private static int recordId = 0;
-    private static int keywordId = 0;
+    private static int recId = 0;
+    private static int kwId = 0;
     private static int iiId = 0;
     private static HashMap<String,Integer> kwToId;
+    private static PrintWriter recOut;
+    private static PrintWriter kwOut;
+    private static PrintWriter iiOut;
+
 
     public static void main(String[] args) {
         kwToId = new HashMap<String,Integer>();
-        if(args.length != 1) {
+        if(args.length != 2) {
             usage();
             System.exit(1);
         }
         try {
+            recOut = new PrintWriter(new BufferedWriter(new FileWriter(args[1] + ".rec.csv")));
+            iiOut = new PrintWriter(new BufferedWriter(new FileWriter(args[1] + ".ii.csv")));
+            kwOut = new PrintWriter(new BufferedWriter(new FileWriter(args[1] + ".kw.csv")));
             BufferedReader f = new BufferedReader(new FileReader(args[0]));
             int i = 0;
             String l;
@@ -25,9 +35,13 @@ public class Main {
                 indexLine(l);
                 i++;
             }
+            recOut.close();
+            iiOut.close();
+            kwOut.close();
         }
         catch (Exception e) {
             System.err.println(e.toString());
+            e.printStackTrace();
             System.exit(1);
         }
     }
@@ -36,7 +50,8 @@ public class Main {
         String keywords[] = line.toLowerCase().split("\\W+");
         Arrays.sort(keywords);
         if(keywords.length < 1) return;
-        recordId ++;
+        recId ++;
+        indexRec(line);
         String curr = null;
         int count = 0;
         for(String kw : keywords) {
@@ -57,22 +72,35 @@ public class Main {
     }
 
     private static void indexKw(String kw, int count) {
-        int kwId = keywordId(kw);
-        System.out.println("Record[" + recordId + "]  Keyword[" + kwId + "] " + kw + " (" + count + ")");
+        Integer id = getKwId(kw);
+        iiId++;
+        iiOut.println("" + iiId + "," + id.toString() + "," + recId + "," + count );
+        // System.out.println("Record[" + recId + "]  Keyword[" + id + "] " + kw + " (" + count + ")");
     }
 
-    private static int keywordId(String kw) {
-        Integer kwId = kwToId.get(kw);
-        if(kwId == null) {
-            keywordId++;
-            kwId = new Integer(keywordId);
-            kwToId.put(kw,kwId);
+    private static Integer getKwId(String kw) {
+        Integer id = kwToId.get(kw);
+        if(id == null) {
+            kwId++;
+            id = new Integer(kwId);
+            kwToId.put(kw,id);
+            kwOut.println(kw + "," + kwId);
         }
-        return kwId.intValue();
+        return id;
+    }
+
+    private static void indexRec(String rec) {
+        String words[] = rec.split("\\W+");
+        recOut.print("" + recId + ",");
+        for(int i=0;i<words.length;i++) {
+            if(i>0) recOut.print(" ");
+            recOut.print(words[i]);
+        }
+        recOut.println("," + rec.length());
     }
 
     private static void usage() {
-        System.err.println("usage: dbidx <input file>");
+        System.err.println("usage: dbidx <input file> <output prefix>");
     }
 
 }
